2e2e02f765f2710675fce9c948c02a75a0ebd71f
Eric Marcon <eric.marcon@ecofog.gf>
2017-12-26 17:09:55 -0300

    Vignettes updated

--- inst/doc/entropart.R
+++ inst/doc/entropart.R
@@ -2,9 +2,93 @@
 library("knitr")
 opts_knit$set(concordance=TRUE)
 # set global chunk options
-opts_chunk$set(cache=TRUE, warning=FALSE, tidy=TRUE, fig.width=8, fig.height=6, out.width='.4\\maxwidth', tidy.opts=list(blank=FALSE, width.cutoff=60), size="scriptsize")
+opts_chunk$set(cache=TRUE, warning=FALSE, tidy=TRUE, fig.width=8, fig.height=6, out.width='.6\\maxwidth', tidy.opts=list(blank=FALSE, width.cutoff=60), size="scriptsize")
 options(width=60)
 par(mar=c(0,0,0,0))
+
+## ----MetaCommunity----------------------------------------
+library("entropart")
+df <- data.frame(C1=c(10, 10, 10, 10), C2=c(0, 20, 35, 5), C3=c(25, 15, 0, 2))
+row.names(df) <- c("sp1", "sp2", "sp3", "sp4")
+df
+w <- c(1, 2, 1)
+MC <- MetaCommunity(Abundances=df, Weights=w)
+
+## ----MCPlot, echo=FALSE, results='hide'-------------------
+par(mar=c(2.5, 4, 1, 0))
+plot(MC)
+
+## ----Ps---------------------------------------------------
+MC$Ps
+
+## ----data-------------------------------------------------
+data("Paracou618")
+summary(Paracou618.MC)
+
+## ----AbdVector--------------------------------------------
+data("Paracou618")
+PAbd <- as.AbdVector(Paracou618.MC$Ns)
+
+## ----ProbaVector------------------------------------------
+PProba <- as.ProbaVector(Paracou618.MC$Ps)
+
+## ----ProbaVectorPlot, echo=FALSE, results='hide'----------
+plot(PAbd)
+
+## ----lnq, echo=FALSE, results='hide'----------------------
+curve(log(x), 0, 1, lty=1, ylab = expression(ln[q](x)))
+curve(lnq(x, 0), 0, 1, lty = 2, add = TRUE)
+curve(lnq(x, 2), 0, 1, lty = 3, add = TRUE)
+curve(lnq(x, 3), 0, 1, lty = 4, add = TRUE)  
+legend("bottomright", legend = c(expression(ln[0](x)), "ln(x)", expression(ln[2](x)), expression(ln[3](x))), lty = c(2, 1, 3, 4), inset=  0.02)
+
+## ----ShannonP6.1------------------------------------------
+Tsallis(PProba, q = 1)
+
+## ----ShannonP6.2------------------------------------------
+Shannon(PProba)
+
+## ----Coverage---------------------------------------------
+Coverage(PAbd)
+
+## ----bcShannonP6.1----------------------------------------
+bcTsallis(PAbd, q = 1)
+
+## ----bcShannonP6.3----------------------------------------
+Tsallis(PAbd, q = 1)
+
+## ----bcShannonP6.4----------------------------------------
+Tsallis(PProba, q = 1)
+
+## ----Diversity--------------------------------------------
+expq(Tsallis(PAbd, q = 2), q = 2)
+Diversity(PAbd, q = 2)
+
+## ----Hurlbert---------------------------------------------
+Hurlbert(PProba, k = 2)
+Hurlbert(PAbd, k = 2)
+HurlbertD(PAbd, k = 2)
+
+## ----AlphaEntropy-----------------------------------------
+e <- AlphaEntropy(Paracou618.MC, q = 1)
+summary(e)
+
+## ----DivPart----------------------------------------------
+p <- DivPart(q = 1, MC = Paracou618.MC, Biased = FALSE)
+summary(p)
+p$CommunityAlphaEntropies
+
+## ----DivPartPlot, echo=FALSE, results='hide'--------------
+par(mar=c(4, 4, 2, 1))
+plot(p)
+
+## ----DivEst-----------------------------------------------
+de <- DivEst(q = 1, Paracou618.MC, Biased = FALSE, Correction = "Best", Simulations = 100)
+summary(de)
+
+## ----DivEstPlot, echo=FALSE, results='hide', out.width='.8\\maxwidth'----
+par(mar=c(4, 4, 2, 1))
+plot(de)
 
 ## ----DivProfile-------------------------------------------
 dp <- DivProfile(seq(0, 2, 0.2), Paracou618.MC, Biased = FALSE, NumberOfSimulations = 10)
