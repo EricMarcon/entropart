47a732b6e9442aff3e82ee560eef78905dd44c9e
Eric Marcon <eric.marcon@ecofog.gf>
2018-02-12 15:45:54 -0300

    Named values not equal in testthat

--- tests/testthat/testHurlbert.R
+++ tests/testthat/testHurlbert.R
@@ -7,7 +7,7 @@ testthat::test_that("Hurlbert diversity is correct", {
   testthat::skip_on_cran()
 
   # Order 2
-  testthat::expect_equal(HurlbertD(Paracou618.MC$Ps, 2), expq(Simpson(Paracou618.MC$Ps), 2))
+  testthat::expect_equal(as.numeric(HurlbertD(Paracou618.MC$Ps, 2)), as.numeric(expq(Simpson(Paracou618.MC$Ps), 2)))
   testthat::expect_lt(bcHurlbertD(Paracou618.MC$Ns, 2) - Diversity(Paracou618.MC$Ns, q=2), 1)
 })
 
