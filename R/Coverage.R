16ca2dbb4457ec916dc8e92e82d0b1ec5bc93200
Eric Marcon <eric.marcon@ecofog.gf>
2018-04-28 16:33:30 -0300

    Coverage at a level

--- R/Coverage.R
+++ R/Coverage.R
@@ -1,5 +1,5 @@
 Coverage <-
-function(Ns, Estimator = "ZhangHuang", CheckArguments = TRUE) 
+function(Ns, Estimator = "Best", Level = NULL, CheckArguments = TRUE) 
 {
   if (CheckArguments)
     CheckentropartArguments()
@@ -11,55 +11,102 @@ function(Ns, Estimator = "ZhangHuang", CheckArguments = TRUE)
   # Calculate abundance distribution
   DistN <- tapply(Ns, Ns, length)
   Singletons <- DistN["1"]
-
-  # No singletons, C=1
-  if (is.na(Singletons)) {
-    coverage <- 1
-    names(coverage) <- "No singleton"
-    return (coverage)
-  }
-  
-  # Abundances
-  Nu <- as.integer(names(DistN))
   SampleSize <- sum(Ns)
-
-  # Singletons only
-  if (Singletons == SampleSize) {
-    warning ("Sample coverage is 0, most bias corrections will return NaN.")
-    coverage <- 0
-    names(coverage) <- "Singletons only"
-    return (coverage)
-  }
-
-  if (Estimator == "ZhangHuang") {
-    Ps <- Ns/SampleSize
-    if (any(Ps >= .5)) {
-      warning ("Zhang-Huang sample coverage cannot be estimated because one probability is over 1/2. Chao estimator is returned.")
-      Estimator <- "Chao"
-    } else {
-      # Use Nu%%2*2-1 for (-1)^(Nu+1)
-      coverage <- 1 - sum((Nu%%2*2-1) / choose(SampleSize, Nu) * DistN)
+  
+  if (is.na(Level)) {
+    # Etimate C at the observed level
+    
+    if (Estimator == "Best") Estimator <- "ZhangHuang"
+    
+    # No singletons, C=1
+    if (is.na(Singletons)) {
+      coverage <- 1
+      names(coverage) <- "No singleton"
+      return(coverage)
+    }
+    
+    # Singletons only
+    if (Singletons == SampleSize) {
+      warning ("Sample coverage is 0, most bias corrections will return NaN.")
+      coverage <- 0
+      names(coverage) <- "Singletons only"
+      return(coverage)
+    }
+  
+    if (Estimator == "ZhangHuang") {
+      Ps <- Ns/SampleSize
+      if (any(Ps >= .5)) {
+        warning ("Zhang-Huang sample coverage cannot be estimated because one probability is over 1/2. Chao estimator is returned.")
+        Estimator <- "Chao"
+      } else {
+        Nu <- as.integer(names(DistN))
+        # Use Nu%%2*2-1 for (-1)^(Nu+1)
+        coverage <- 1 - sum((Nu%%2*2-1) / choose(SampleSize, Nu) * DistN)
+        names(coverage) <- Estimator
+        return(coverage)
+      }    
+    }
+    if (Estimator == "Chao") {
+      coverage <- 1 - Singletons / SampleSize * (1-ChaoA)
+      return(coverage)
+    }
+    if (Estimator == "Turing") {
+      coverage <- 1 - Singletons / SampleSize
       names(coverage) <- Estimator
-      return (coverage)
-    }    
-  }
-  if (Estimator == "Chao") {
-    Doubletons <- DistN["2"]
-    if (is.na(Doubletons)) {
-      warning ("Chao's sample coverage cannot be estimated because there are no doubletons. Turing estimator is returned.")
-      Estimator <- "Turing"
-    } else {
-      coverage <- 1 - Singletons / SampleSize *((SampleSize - 1) * Singletons / ((SampleSize - 1) * Singletons + 2 * Doubletons))
-      names(coverage) <- Estimator
-      return (coverage)
+      return(coverage)
+    }
+    
+  } else {
+    # Chose level. Must be an integer. CheckEntropartArguments() may have accepted a value between 0 and 1
+    if (Level <=1) stop("Level must be an integer >1.")
+
+    if (Estimator == "Best") Estimator <- "Good"
+    
+    if (Estimator == "Good") {
+      if (Level >= SampleSize) stop("The Good estimator only allows interpolation: Level must be less than the observed community size.")
+      coverage <- 1 - EntropyEstimation::GenSimp.z(Ns, Level)
+      return(coverage)
+    }
+    if (Estimator == "Chao") {
+      if (Level < SampleSize) {
+        # Interpolation
+        NsRestricted <- Ns[(SampleSize - Ns) >= Level]
+        coverage <- 1 - sum(NsRestricted/SampleSize 
+          * exp(lgamma(SampleSize - NsRestricted + 1) - lgamma(SampleSize - NsRestricted - Level + 1) - lgamma(SampleSize) + lgamma(SampleSize - Level)))
+      } else {
+        # Extrapolation
+        coverage <- 1 - Singletons / SampleSize * (1 - ChaoA(Ns))^(Level - SampleSize + 1)
+      }
+      return(coverage)
     }
   }
-  if (Estimator == "Turing") {
-    coverage <- 1 - Singletons / SampleSize
-    names(coverage) <- Estimator
-    return (coverage)
-  }
+  
   warning("Correction has not been recognized")
-  return (NA)
+  return(NA)
+}
 
+
+
+# Helper for Chao's estimator. Not exported.
+# A's formula depends on the presence of singletons and doubletons.
+# Ns must be a vector of positive integers (not checked).
+ChaoA <- function(Ns) {
+  # Calculate abundance distribution
+  DistN <- tapply(Ns, Ns, length)
+  Singletons <- DistN["1"]
+  Doubletons <- DistN["2"]
+  SampleSize <- sum(Ns)
+  
+  if (is.na(Singletons)) {
+    A <- 0
+  } else {
+    if (is.na(Doubletons)) {
+      Chao1S0 <- (SampleSize - 1) * Singletons * (Singletons - 1) / 2 / SampleSize
+    } else {
+      Chao1S0 <- (SampleSize - 1) * Singletons^2 / 2 / SampleSize / Doubletons
+    }
+    A <- 1- SampleSize * Chao1S0/(SampleSize * Chao1S0 + Singletons)
+  }
+  
+  return(A)
 }
--- R/GenSimpson.R
+++ R/GenSimpson.R
@@ -1,8 +1,8 @@
 GenSimpson <-
-  function(NorP, r = 1, ...) 
-  {
-    UseMethod("GenSimpson")
-  }
+function(NorP, r = 1, ...) 
+{
+  UseMethod("GenSimpson")
+}
 
 
 GenSimpson.ProbaVector <-
@@ -39,42 +39,42 @@ function(NorP, r = 1, ..., CheckArguments = TRUE, Ns = NULL)
 
 
 GenSimpson.integer <-
-  function(NorP, r = 1, ..., CheckArguments = TRUE, Ns = NULL)
-  {
-    if (missing(NorP)){
-      if (!missing(Ns)) {
-        NorP <- Ns
-      } else {
-        stop("An argument NorP or Ns must be provided.")
-      }
+function(NorP, r = 1, ..., CheckArguments = TRUE, Ns = NULL)
+{
+  if (missing(NorP)){
+    if (!missing(Ns)) {
+      NorP <- Ns
+    } else {
+      stop("An argument NorP or Ns must be provided.")
     }
-    return(bcGenSimpson(Ns=NorP, r=r, CheckArguments=CheckArguments))
   }
+  return(bcGenSimpson(Ns=NorP, r=r, CheckArguments=CheckArguments))
+}
 
 
 GenSimpson.numeric <-
-  function(NorP, r = 1, ..., CheckArguments = TRUE, Ps = NULL, Ns = NULL) 
-  {
-    if (missing(NorP)){
-      if (!missing(Ps)) {
-        NorP <- Ps
+function(NorP, r = 1, ..., CheckArguments = TRUE, Ps = NULL, Ns = NULL) 
+{
+  if (missing(NorP)){
+    if (!missing(Ps)) {
+      NorP <- Ps
+    } else {
+      if (!missing(Ns)) {
+        NorP <- Ns
       } else {
-        if (!missing(Ns)) {
-          NorP <- Ns
-        } else {
-          stop("An argument NorP or Ps or Ns must be provided.")
-        }
+        stop("An argument NorP or Ps or Ns must be provided.")
       }
     }
-    
-    if (abs(sum(NorP) - 1) < length(NorP)*.Machine$double.eps) {
-      # Probabilities sum to 1, allowing rounding error
-      return(GenSimpson.ProbaVector(NorP, r=r, CheckArguments=CheckArguments))
-    } else {
-      # Abundances
-      return(GenSimpson.AbdVector(NorP, r=r, CheckArguments=CheckArguments))
-    }
   }
+  
+  if (abs(sum(NorP) - 1) < length(NorP)*.Machine$double.eps) {
+    # Probabilities sum to 1, allowing rounding error
+    return(GenSimpson.ProbaVector(NorP, r=r, CheckArguments=CheckArguments))
+  } else {
+    # Abundances
+    return(GenSimpson.AbdVector(NorP, r=r, CheckArguments=CheckArguments))
+  }
+}
 
 
 bcGenSimpson <-
